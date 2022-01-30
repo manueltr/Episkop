f460199dd7532c424590cafe4ca5591e9bdcc590
manueltr <manuelraul5@gmail.com>
2022-01-30 19:10:11 +0000

    added test for session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
   skip_before_action :require_login
   
-  def new
-  end
+  # def new
+  # end
 
-  def create 
-  end
+  # def create 
+  # end
 
   def destroy
     puts "hello"
--- spec/requests/sessions_controller_spec.rb
+++ spec/requests/sessions_controller_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "SessionsControllers", type: :request do
   
+    it "should logout and redirect the user when getting #destryo" do
+        get '/logout'
+        expect(response).to redirect_to(root_path)
+        expect(session[:user_id]).to eq(nil) 
+    end
+
+    it "should set the user and redirect to the homepage" do
+        request.env['omniauth.auth'] = {:uid => "test", :povider => "google"}
+        get '/login'
+        expect(session[:user_id]).to eq(User.find_by(:username => "testUser1").id)
+        expect(response).to redirect_to(logged_in_path)
+    end
 end
