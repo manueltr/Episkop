552f26a38d9305c9b7967965fd58cc1367b9027f
manueltr <manuelraul5@gmail.com>
2022-01-30 22:19:29 +0000

    added testing for creation of a user

--- spec/controllers/sessions_controllers_spec.rb
+++ spec/controllers/sessions_controllers_spec.rb
@@ -5,9 +5,20 @@ RSpec.describe "SessionsControllers", type: :controller do
     end
 
     it "should sign in the user" do 
-        request.env['omniauth.auth'] = {uid: "test", provide: "google"}
+        request.env['omniauth.auth'] = {uid: "test", provider: "google"}
         get :omniauth, params: {:provider => 'google'}
         expect(session[:user_id]).to eq(User.find_by(:username => "testUser1").id)
         expect(response).to redirect_to(logged_in_path)
-    end 
+    end
+
+    it "should create a new user if it doesnt exit and redirect to logged in path" do
+        request.env['omniauth.auth'] = {uid: "testing123", 
+                                        provider: "google",
+                                        info: { name: "testUser4", first_name: "",
+                                                email: "", image: ""}}
+        get :omniauth, params: {:provider => 'google'}
+        expect(session[:user_id]).to eq(User.find_by(:username => "testUser4").id)
+        expect(response).to redirect_to(logged_in_path)
+        delete :destroy, params: {:id => User.find_by(:username => "testUser4").id}
+    end
 end
